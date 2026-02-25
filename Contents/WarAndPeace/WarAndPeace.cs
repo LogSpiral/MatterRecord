3810167203d6314b816a9e9ce0e854a0de3a46e6
错数螺线 <3253615697@qq.com>
2026-02-25 22:53:30 +0800

    修复了战争与和平未解锁依旧可以装备并生效的问题

--- Contents/WarAndPeace/WarAndPeace.cs
+++ Contents/WarAndPeace/WarAndPeace.cs
@@ -1,11 +1,9 @@
 ﻿using MatterRecord.Contents.Recorder;
-using Microsoft.Xna.Framework;
 using System;
-using Terraria.GameContent;
 
 namespace MatterRecord.Contents.WarAndPeace;
 
-public class WarAndPeace : ModItem,IRecordBookItem
+public class WarAndPeace : ModItem, IRecordBookItem
 {
     ItemRecords IRecordBookItem.RecordType => ItemRecords.WarAndPeace;
     public override void SetDefaults()
@@ -24,6 +22,7 @@ public class WarAndPeace : ModItem,IRecordBookItem
 
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
+        if (!this.IsRecordUnlocked) return;
         var dayOfWeek = DateTime.Now.DayOfWeek;
         if (dayOfWeek == DayOfWeek.Sunday)
             player.AddBuff(IsPeace ? ModContent.BuffType<Holiday_Peace>() : ModContent.BuffType<Holiday_War>(), 2);
@@ -40,4 +39,5 @@ public class WarAndPeace : ModItem,IRecordBookItem
         }
         base.UpdateAccessory(player, hideVisual);
     }
+    public override bool CanEquipAccessory(Player player, int slot, bool modded) => this.IsRecordUnlocked;
 }
