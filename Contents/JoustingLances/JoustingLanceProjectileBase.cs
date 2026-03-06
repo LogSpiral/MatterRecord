548abf19ef4bc0c9bb41bd64ffbd94b46f29659c
错数螺线 <3253615697@qq.com>
2026-03-06 08:12:17 +0800

    现在骑枪需要击杀敌对NPC才能出堂吉诃德

--- Contents/JoustingLances/JoustingLanceProjectileBase.cs
+++ Contents/JoustingLances/JoustingLanceProjectileBase.cs
@@ -81,8 +81,7 @@ public abstract class JoustingLanceProjectileBase : ModProjectile
     }
     public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
     {
-        Main.NewText(target.life);
-        if (target.life < 0 && RecorderSystem.ShouldSpawnRecordItem<DonQuijoteDeLaMancha.DonQuijoteDeLaMancha>())
+        if (!target.friendly && target.life < 0 && RecorderSystem.ShouldSpawnRecordItem<DonQuijoteDeLaMancha.DonQuijoteDeLaMancha>())
             Main.LocalPlayer.QuickSpawnItem(target.GetItemSource_Loot(), ModContent.ItemType<DonQuijoteDeLaMancha.DonQuijoteDeLaMancha>());
     }
     public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
