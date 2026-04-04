d66a10b0f74b8eb056b5c2c55ed8ca9438278e7f
错数螺线 <3253615697@qq.com>
2026-04-05 00:08:54 +0800

    添加了《本草纲目》

---
+++ Contents/CompendiumOfMateriaMedica/CompendiumGlobalItem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace MatterRecord.Contents.CompendiumOfMateriaMedica
+{
+    public class CompendiumGlobalItem : GlobalItem
+    {
+        private static readonly HashSet<int> HerbIds = new HashSet<int>
+        {
+            ItemID.Blinkroot,
+            ItemID.Daybloom,
+            ItemID.Deathweed,
+            ItemID.Fireblossom,
+            ItemID.Moonglow,
+            ItemID.Shiverthorn,
+            ItemID.Waterleaf
+        };
+
+        public override bool? UseItem(Item item, Player player)
+        {
+            var compPlayer = player.GetModPlayer<CompendiumPlayer>();
+            // 如果玩家自己装备或同队有人装备，则触发药水效果
+            if (compPlayer.ShouldGainHerbEffects())
+            {
+                if (item.consumable && item.buffType > 0 && item.buffTime > 0)
+                {
+                    List<Recipe> recipes = GetRecipesForItem(item.type);
+                    bool hasHerb = false;
+                    foreach (var recipe in recipes)
+                    {
+                        foreach (var requiredItem in recipe.requiredItem)
+                        {
+                            if (HerbIds.Contains(requiredItem.type))
+                            {
+                                compPlayer.ActivatePotionEffect(requiredItem.type);
+                                hasHerb = true;
+                            }
+                        }
+                    }
+                    if (hasHerb)
+                    {
+                        player.AddBuff(ModContent.BuffType<FragrantHerbs>(), 300 * 60);
+                    }
+                }
+            }
+            return base.UseItem(item, player);
+        }
+
+        private List<Recipe> GetRecipesForItem(int itemType)
+        {
+            var recipes = new List<Recipe>();
+            for (int i = 0; i < Main.recipe.Length; i++)
+            {
+                if (Main.recipe[i].createItem.type == itemType)
+                    recipes.Add(Main.recipe[i]);
+            }
+            return recipes;
+        }
+    }
+}
---
+++ Contents/CompendiumOfMateriaMedica/FragrantHerbsBuff.cs
@@ -0,0 +1,13 @@
+﻿namespace MatterRecord.Contents.CompendiumOfMateriaMedica
+{
+    public class FragrantHerbs : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.debuff[Type] = false;
+            Main.buffNoSave[Type] = false;
+            Main.buffNoTimeDisplay[Type] = false;
+        }
+    }
+}
