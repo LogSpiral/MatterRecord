3de1ef926858176d5c54afe8f33e5b5cc8aaaea8
错数螺线 <3253615697@qq.com>
2026-04-10 22:15:54 +0800

    修复了自由论的同步问题

--- Contents/TheoryOfFreedom/HookPointSync.cs
+++ Contents/TheoryOfFreedom/HookPointSync.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.Xna.Framework;
 using NetSimplified;
 using NetSimplified.Syncing;
+using System.IO;
 
 namespace MatterRecord.Contents.TheoryOfFreedom;
 
-[AutoSync]
 internal class HookPointSync : NetModule
 {
     private byte _whoAmI;
     private Point[] _coords;
+    public override void Send(ModPacket p)
+    {
+        var length = (byte)_coords.Length;
+        p.Write(_whoAmI);
+        p.Write(length);
+        for (byte n = 0; n < length; n++) 
+        {
+            var coord = _coords[n];
+            p.Write(coord.X);
+            p.Write(coord.Y);
+        }
+    }
+    public override void Read(BinaryReader r)
+    {
+        _whoAmI = r.ReadByte();
+        int length = r.ReadByte();
+        _coords = new Point[length];
+        for (int n = 0; n < length; n++)
+            _coords[n] = new(r.ReadInt32(), r.ReadInt32());
+    }
     public static HookPointSync Get(int whoAmI, Point[] coords)
     {
         var packet = NetModuleLoader.Get<HookPointSync>();
